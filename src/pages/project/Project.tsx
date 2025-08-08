fd7d42abbd6efa393947f0f8b59986d13e4b6657
Romain-dev85 <r.auvinet@gmail.com>
2025-08-08 18:21:10 +0200

    project page done

--- src/pages/project/Project.tsx
+++ src/pages/project/Project.tsx
@@ -12,10 +12,10 @@ const Project = () => {
 	return (
 		<div className="project-container">
 			<h1>PROJETS</h1>
-			<p>
+			<h2>
 				Vous trouverez ici des projets que j'ai crée en formation (en groupe ou
 				en solo), mais également mes propres projets
-			</p>
+			</h2>
 			<section>
 				<article>
 					<div>
@@ -24,34 +24,36 @@ const Project = () => {
 							alt="screenshot Gard'Malin website"
 						/>
 					</div>
-					<h2>Gard'Malin</h2>
-					<a
-						href="https://github.com/wildcodeschool-2025-03/JS-WestDevs-P3-GardMalin"
-						target="blank"
-					>
-						<SiGithub />
-					</a>
-					<p>
-						Site web de réservation de crèche. L’objectif était de créer un site
-						afin de pouvoir reservé une place en crèche pour un enfant
-					</p>
-					<ul>
-						<li>
-							<SiHtml5 />
-						</li>
-						<li>
-							<SiCss3 />
-						</li>
-						<li>
-							<SiJavascript />
-						</li>
-						<li>
-							<SiReact />
-						</li>
-						<li>
-							<SiMysql />
-						</li>
-					</ul>
+					<div>
+						<h3>Gard'Malin</h3>
+						<a
+							href="https://github.com/wildcodeschool-2025-03/JS-WestDevs-P3-GardMalin"
+							target="blank"
+						>
+							<SiGithub />
+						</a>
+						<p>
+							Site web de réservation de crèche. L’objectif était de créer un
+							site afin de pouvoir reservé une place en crèche pour un enfant
+						</p>
+						<ul>
+							<li>
+								<SiHtml5 />
+							</li>
+							<li>
+								<SiCss3 />
+							</li>
+							<li>
+								<SiJavascript />
+							</li>
+							<li>
+								<SiReact />
+							</li>
+							<li>
+								<SiMysql />
+							</li>
+						</ul>
+					</div>
 				</article>
 				<article>
 					<div>
@@ -60,31 +62,33 @@ const Project = () => {
 							alt="screenshot Savor Safari website"
 						/>
 					</div>
-					<h2>Savor Safari</h2>
-					<a
-						href="https://github.com/wildcodeschool-2025-03/JS-WestDevs-P2-SavorSafari"
-						target="blank"
-					>
-						<SiGithub />
-					</a>
-					<p>
-						Site web permettant d’afficher une liste de recette en sélectionnant
-						un pays
-					</p>
-					<ul>
-						<li>
-							<SiHtml5 />
-						</li>
-						<li>
-							<SiCss3 />
-						</li>
-						<li>
-							<SiJavascript />
-						</li>
-						<li>
-							<SiReact />
-						</li>
-					</ul>
+					<div>
+						<h3>Savor Safari</h3>
+						<a
+							href="https://github.com/wildcodeschool-2025-03/JS-WestDevs-P2-SavorSafari"
+							target="blank"
+						>
+							<SiGithub />
+						</a>
+						<p>
+							Site web permettant d’afficher une liste de recette en
+							sélectionnant un pays
+						</p>
+						<ul>
+							<li>
+								<SiHtml5 />
+							</li>
+							<li>
+								<SiCss3 />
+							</li>
+							<li>
+								<SiJavascript />
+							</li>
+							<li>
+								<SiReact />
+							</li>
+						</ul>
+					</div>
 				</article>
 				<article>
 					<div>
@@ -93,31 +97,36 @@ const Project = () => {
 							alt="screenshot Iconic Album Cover website"
 						/>
 					</div>
-					<h2>Iconic Album Cover</h2>
-					<a href="https://github.com/Romain-dev85/album_cover" target="blank">
-						<SiGithub />
-					</a>
-					<p>
-						Site web permettant de réaliser un CRUD complet. Possibilité
-						d’ajouter une pochette d’album, de la modifier et de la supprimer.
-					</p>
-					<ul>
-						<li>
-							<SiHtml5 />
-						</li>
-						<li>
-							<SiCss3 />
-						</li>
-						<li>
-							<SiJavascript />
-						</li>
-						<li>
-							<SiReact />
-						</li>
-						<li>
-							<SiMysql />
-						</li>
-					</ul>
+					<div>
+						<h3>Iconic Album Cover</h3>
+						<a
+							href="https://github.com/Romain-dev85/album_cover"
+							target="blank"
+						>
+							<SiGithub />
+						</a>
+						<p>
+							Site web permettant de réaliser un CRUD complet. Possibilité
+							d’ajouter une pochette d’album, de la modifier et de la supprimer.
+						</p>
+						<ul>
+							<li>
+								<SiHtml5 />
+							</li>
+							<li>
+								<SiCss3 />
+							</li>
+							<li>
+								<SiJavascript />
+							</li>
+							<li>
+								<SiReact />
+							</li>
+							<li>
+								<SiMysql />
+							</li>
+						</ul>
+					</div>
 				</article>
 			</section>
 		</div>
