3a0681ecb2b8a19295fcb950dd75064847a36254
Romain-dev85 <r.auvinet@gmail.com>
2025-08-08 19:09:07 +0200

    work done

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -16,49 +16,60 @@ const Home = () => {
 	return (
 		<div className="home-container">
 			<h1>BIENVENUE, JE SUIS ROMAIN AUVINET</h1>
-			<p>
-				"Passionné par un code propre et soigné, je mène chaque projet à terme
-				avec rigueur et exigence."
-			</p>
-			<h2>MES COMPETENCES</h2>
-			<ul>
-				<li>
-					<SiReact />
-					React
-				</li>
-				<li>
-					<SiJavascript />
-					Javascript
-				</li>
-				<li>
-					<SiTypescript />
-					Typescript
-				</li>
-				<li>
-					<SiNodedotjs />
-					Node.js
-				</li>
-				<li>
-					<SiExpress />
-					Express.js
-				</li>
-				<li>
-					<SiMysql />
-					Mysql
-				</li>
-				<li>
-					<SiGit />
-					Git
-				</li>
-				<li>
-					<SiHtml5 />
-					Html
-				</li>
-				<li>
-					<SiCss3 />
-					Css
-				</li>
-			</ul>
+			<h2>
+				"Ici, je partage mon univers de développeur : mes compétences
+				techniques, mes projets, et la passion qui m’anime au quotidien."
+			</h2>
+			<section>
+				<article>
+					<p>
+						"Je m’épanouis aussi dans la musique, le sport et la nature. Que ce
+						soit derrière un écran ou sur un vélo, j’aime aller au bout des
+						choses avec énergie et enthousiasme."
+					</p>
+				</article>
+				<article>
+					<h2>MES COMPETENCES</h2>
+					<ul>
+						<li>
+							<SiReact />
+							React
+						</li>
+						<li>
+							<SiJavascript />
+							Javascript
+						</li>
+						<li>
+							<SiTypescript />
+							Typescript
+						</li>
+						<li>
+							<SiNodedotjs />
+							Node.js
+						</li>
+						<li>
+							<SiExpress />
+							Express.js
+						</li>
+						<li>
+							<SiMysql />
+							Mysql
+						</li>
+						<li>
+							<SiGit />
+							Git
+						</li>
+						<li>
+							<SiHtml5 />
+							Html
+						</li>
+						<li>
+							<SiCss3 />
+							Css
+						</li>
+					</ul>
+				</article>
+			</section>
 			<nav>
 				<Link to="/project">PROJETS</Link>
 				<Link to="">CONTACT</Link>
