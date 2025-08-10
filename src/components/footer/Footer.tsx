d450b744893bbb5edb8edc0d48c37092b140393b
Romain-dev85 <r.auvinet@gmail.com>
2025-08-10 14:23:12 +0200

    work done

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,12 +5,17 @@ const Footer = () => {
 		<div className="footer-container">
 			<p>© Copyright 2025. Réalisé par Romain Auvinet</p>
 			<nav>
-				<a href="https://github.com/Romain-dev85" target="blank">
+				<a
+					href="https://github.com/Romain-dev85"
+					target="blank"
+					rel="noopener noreferrer"
+				>
 					<img src="/assets/images/github.svg" alt="logo linkedin" />
 				</a>
 				<a
 					href="https://linkedin.com/in/romain-auvinet-103793328"
 					target="blank"
+					rel="noopener noreferrer"
 				>
 					<img src="/assets/images/linkedin.svg" alt="logo github" />
 				</a>
