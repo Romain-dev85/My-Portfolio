9ea64f3fc5b5fb9155408c0515a7fdb1814ac17c
Romain-dev85 <r.auvinet@gmail.com>
2025-08-10 18:35:32 +0200

    form ok

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -2,74 +2,77 @@ import React from "react";
 import emailjs from "@emailjs/browser";
 import "./Contact.css";
 
-
-
 const Contact = () => {
-  
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    
-    const EMAILJS_PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY || "";
+	const lengthMinMessage = 10;
+	const lengthMaxMessage = 400;
+	const lengthMaxName = 50;
 
-    const formData = new FormData(event.currentTarget);
-    const formObj = Object.fromEntries(formData) as {
-      name?: string;
-      email?: string;
-      message?: string;
-    };
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
 
-    try {
-      await emailjs.send(
-        "service_ogcl9gr",
-        "template_ogcl9gr",
-        {
-          name: formObj.name || "",
-          email: formObj.email || "",
-          message: formObj.message || "",
-        }, 
-         EMAILJS_PUBLIC_KEY
-      );
-      event.currentTarget.reset();
-    } catch (err) {
-      console.error("Erreur lors de l’envoi du mail :", err);
-    }
-  };
+		const EMAILJS_PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY || "";
 
-  return (
+		const formData = new FormData(event.currentTarget);
+		const formObj = Object.fromEntries(formData) as {
+			name?: string;
+			email?: string;
+			message?: string;
+		};
 
-    <div className="contact-container">
-      <h1>CONTACT</h1>
-      <form className="contact-form" onSubmit={handleSubmit}>
-        <label htmlFor="Nom">Nom
-        <input type="text" placeholder="Nom" name="name" required />
-        </label>
-        <label htmlFor="Email">Email
-        <input type="email" placeholder="Adresse mail" name="email" required />
-        </label>
-        <label htmlFor="Message">Message
-        <textarea name="message" placeholder="Message" required></textarea>
-        </label>
-        <input type="submit" value="Envoyer" id="input-submit" />
-      </form>
-    </div>
-  );
+		try {
+			await emailjs.send(
+				"service_ogcl9gr",
+				"template_ogcl9gr",
+				{
+					name: formObj.name,
+					email: formObj.email,
+					message: formObj.message,
+				},
+				EMAILJS_PUBLIC_KEY,
+			);
+			event.currentTarget.reset();
+		} catch (err) {
+			console.error("Erreur lors de l’envoi du mail :", err);
+		}
+	};
+
+	return (
+		<div className="contact-container">
+			<h1>CONTACT</h1>
+			<form className="contact-form" onSubmit={handleSubmit}>
+				<label htmlFor="name">
+					Nom
+					<input
+						type="text"
+						placeholder="Nom"
+						name="name"
+						required
+						maxLength={lengthMaxName}
+					/>
+				</label>
+				<label htmlFor="Email">
+					Email
+					<input
+						type="email"
+						placeholder="Adresse mail"
+						name="email"
+						required
+					/>
+				</label>
+				<label htmlFor="Message">
+					Message
+					<textarea
+						name="message"
+						placeholder="Message"
+						minLength={lengthMinMessage}
+						maxLength={lengthMaxMessage}
+						required
+					></textarea>
+				</label>
+				<input type="submit" value="Envoyer" id="input-submit" />
+			</form>
+		</div>
+	);
 };
 
 export default Contact;
-//     <div className="contact-container">
-//       <h1>CONTACT</h1>
-//       <form className="contact-form" onSubmit={handleSubmit}>
-//         <div className="half left cf">
-//           <input type="text" placeholder="Nom" name="name" required />
-//           <input type="email" placeholder="Adresse mail" name="email" required />
-//         </div>
-//         <div className="half right cf">
-//           <textarea name="message" placeholder="Message" required></textarea>
-//         </div>
-//         <input type="submit" value="Envoyer" id="input-submit" />
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Contact;
